3eb5dd567879717c8a8a4fbbf63981b5a280f28a
amane1 <amoljmane@live.com>
2024-02-03 09:01:53 -0800

    Diameter of tree

--- java_solutions/src/DiameterOfTree.java
+++ java_solutions/src/DiameterOfTree.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class DiameterOfTree {
+//https://leetcode.com/problems/diameter-of-binary-tree/
+public class DiameterOfTree {
+    private int dia = Integer.MIN_VALUE;
+    public int getDia(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lh = getDia(root.left);
+        int rh = getDia(root.right);
+
+        dia = Math.max(dia, (lh + rh));
+        return Math.max(lh, rh) + 1;
+    }
+
+    public static void main(String[] args) {
+        DiameterOfTree obj = new DiameterOfTree();
+
+        TreeNode root = new TreeNode(1);
+//
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+//
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(6);
+        System.out.println(obj.getDia(root));
+    }
 }
