39064eaf99c679b77830ed4d70cd74729977cec4
amane1 <amoljmane@live.com>
2024-02-03 08:34:14 -0800

    Flatten Tree

---
+++ java_solutions/src/FlattenTree.java
@@ -0,0 +1,62 @@
+
+//https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+public class FlattenTree {
+
+    private TreeNode flattenTree(TreeNode node) {
+        if (node == null) return null;
+
+        if (node.left == null && node.right == null) {
+            return node;
+        }
+
+        TreeNode leftNode = flattenTree(node.left);
+        TreeNode rightNode = flattenTree(node.right);
+        System.out.println("----");
+        System.out.println("Node " + (node == null? "null":node.val));
+        System.out.println("Left Node " + (leftNode == null ? "null": leftNode.val));
+        System.out.println("Right Node " + (rightNode == null ? "null": rightNode.val));
+        System.out.println("node.Left " + (node.left == null ? "null": node.left.val));
+        System.out.println("node.right " + (node.right == null ? "null": node.right.val));
+
+        System.out.println();
+        System.out.println();
+        if (leftNode != null) {
+            leftNode.right = node.right;
+            node.right = node.left;
+            node.left = null;
+        }
+        return rightNode == null ? leftNode:rightNode;
+    }
+
+    public void flatten(TreeNode root) {
+        flattenTree(root);
+    }
+    public static void main(String[] args) {
+        FlattenTree obj = new FlattenTree();
+
+        TreeNode root = new TreeNode(1);
+//
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+//
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(6);
+
+        obj.flatten(root);
+
+    }
+}
